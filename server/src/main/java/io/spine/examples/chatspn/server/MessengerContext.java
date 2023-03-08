ddcfcb2f034e6b680f2ff50b3c2bfa70047a4102
Hellamb <106074440+Vladyslav-Kuksiuk@users.noreply.github.com>
2023-03-08 18:22:50 +0200

    Implement `MessengerContext`.

---
+++ server/src/main/java/io/spine/examples/chatspn/server/MessengerContext.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2023, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.examples.chatspn.server;
+
+import io.spine.examples.chatspn.server.user.UserAggregate;
+import io.spine.server.BoundedContext;
+import io.spine.server.BoundedContextBuilder;
+import io.spine.server.DefaultRepository;
+
+/**
+ * Configures Messenger Bounded Context with repositories.
+ */
+public final class MessengerContext {
+
+    static final String NAME = "Messenger";
+
+    /**
+     * Prevents instantiation of this utility class.
+     */
+    private MessengerContext() {
+    }
+
+    /**
+     * Creates {@code BoundedContextBuilder} for the Messenger context and fills it with
+     * repositories.
+     */
+    public static BoundedContextBuilder newBuilder() {
+        return BoundedContext
+                .singleTenant(NAME)
+                .add(DefaultRepository.of(UserAggregate.class));
+    }
+
+}
