138e993b33a0b871659ef1508221e925932d09da
Hellamb <106074440+Vladyslav-Kuksiuk@users.noreply.github.com>
2023-03-17 18:14:41 +0200

    Implement `projection` reading in `MessageSendingProcess`.
     `MessageSendingProcess` reads `ChatMembers` projection and throws `MessageCannotBeSent` if the message sender isn't a chat member.

--- server/src/main/java/io/spine/examples/chatspn/server/message/MessageSendingProcess.java
+++ server/src/main/java/io/spine/examples/chatspn/server/message/MessageSendingProcess.java
@@ -26,17 +26,29 @@
 
 package io.spine.examples.chatspn.server.message;
 
+import com.google.common.collect.ImmutableSet;
+import com.google.protobuf.Any;
+import io.spine.client.ActorRequestFactory;
+import io.spine.client.Query;
+import io.spine.client.QueryFactory;
+import io.spine.client.QueryResponse;
+import io.spine.core.CommandContext;
 import io.spine.examples.chatspn.MessageId;
+import io.spine.examples.chatspn.chat.ChatMembers;
 import io.spine.examples.chatspn.message.MessageSending;
 import io.spine.examples.chatspn.message.command.PostMessage;
 import io.spine.examples.chatspn.message.event.MessagePosted;
 import io.spine.examples.chatspn.message.sendingcommand.SendMessage;
 import io.spine.examples.chatspn.message.sendingevent.MessageSent;
+import io.spine.examples.chatspn.message.sendingrejection.MessageCannotBeSent;
+import io.spine.grpc.MemoizingObserver;
 import io.spine.server.command.Command;
 import io.spine.server.event.React;
 import io.spine.server.procman.ProcessManager;
 import io.spine.server.stand.Stand;
 import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
+
+import static io.spine.protobuf.AnyPacker.unpack;
 
 /**
  * Coordinates the message sending to the chat.
@@ -53,17 +65,39 @@ public final class MessageSendingProcess
 
     /**
      * Issues a command to post message to the chat.
+     *
+     * @throws MessageCannotBeSent
+     *         if the message sender is not a chat member.
      */
     @Command
-    PostMessage on(SendMessage c) {
+    PostMessage on(SendMessage c, CommandContext ctx) throws MessageCannotBeSent {
         initState(c);
-        return PostMessage
+
+        QueryFactory queryFactory = queryFactoryOnTopOf(ctx);
+        Query query = queryFactory.byIds(
+                ChatMembers.class,
+                ImmutableSet.of(c.getChat())
+        );
+        ChatMembers chatMembers = executeAndUnpackResponse(query);
+
+        if (chatMembers.getMemberList()
+                       .contains(c.getUser())) {
+            return PostMessage
+                    .newBuilder()
+                    .setId(c.getId())
+                    .setChat(c.getChat())
+                    .setUser(c.getUser())
+                    .setContent(c.getContent())
+                    .vBuild();
+        }
+
+        throw MessageCannotBeSent
                 .newBuilder()
                 .setId(c.getId())
                 .setChat(c.getChat())
                 .setUser(c.getUser())
                 .setContent(c.getContent())
-                .vBuild();
+                .build();
     }
 
     private void initState(SendMessage c) {
@@ -83,6 +117,22 @@ public final class MessageSendingProcess
                 .setUser(e.getUser())
                 .setContent(e.getContent())
                 .vBuild();
+    }
+
+    private QueryFactory queryFactoryOnTopOf(CommandContext ctx) {
+        ActorRequestFactory requestFactory =
+                ActorRequestFactory.fromContext(ctx.getActorContext());
+        return requestFactory.query();
+    }
+
+    private ChatMembers executeAndUnpackResponse(Query query) {
+        MemoizingObserver<QueryResponse> observer = new MemoizingObserver<>();
+        stand.execute(query, observer);
+        QueryResponse response = observer.firstResponse();
+        Any packed = response.getMessageList()
+                             .get(0)
+                             .getState();
+        return unpack(packed, ChatMembers.class);
     }
 
     void inject(Stand stand) {
