c02c7a49802735bb9ab5a64fec62c24295eb2112
Vladyslav-Kuksiuk <vladyslav.kuksiuk@teamdev.com>
2023-03-28 19:14:10 +0300

    Rewrite test on `ChatAggregate`.

---
+++ server/src/test/java/io/spine/examples/chatspn/server/given/ChatTestEnv.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2023, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.examples.chatspn.server.given;
+
+import io.spine.examples.chatspn.ChatId;
+import io.spine.examples.chatspn.chat.Chat;
+import io.spine.examples.chatspn.chat.command.CreateGroupChat;
+import io.spine.examples.chatspn.chat.command.CreatePersonalChat;
+import io.spine.examples.chatspn.chat.event.GroupChatCreated;
+import io.spine.examples.chatspn.chat.event.PersonalChatCreated;
+import io.spine.testing.core.given.GivenUserId;
+
+import static io.spine.examples.chatspn.chat.Chat.ChatType.GROUP;
+import static io.spine.examples.chatspn.chat.Chat.ChatType.PERSONAL;
+import static io.spine.testing.TestValues.randomString;
+
+public final class ChatTestEnv {
+
+    public static CreatePersonalChat createPersonalChatCommand() {
+        CreatePersonalChat command = CreatePersonalChat
+                .newBuilder()
+                .setId(ChatId.generate())
+                .setCreator(GivenUserId.generated())
+                .setMember(GivenUserId.generated())
+                .vBuild();
+        return command;
+    }
+
+    public static PersonalChatCreated personalChatCreatedFrom(CreatePersonalChat c) {
+        PersonalChatCreated event = PersonalChatCreated
+                .newBuilder()
+                .setId(c.getId())
+                .setCreator(c.getCreator())
+                .setMember(c.getMember())
+                .vBuild();
+        return event;
+    }
+
+    public static Chat chatFrom(CreatePersonalChat c) {
+        Chat state = Chat
+                .newBuilder()
+                .setId(c.getId())
+                .setType(PERSONAL)
+                .addMember(c.getCreator())
+                .addMember(c.getMember())
+                .vBuild();
+        return state;
+    }
+
+    public static CreateGroupChat createGroupChatCommand() {
+        CreateGroupChat command = CreateGroupChat
+                .newBuilder()
+                .setId(ChatId.generate())
+                .setCreator(GivenUserId.generated())
+                .addMember(GivenUserId.generated())
+                .addMember(GivenUserId.generated())
+                .setName(randomString())
+                .vBuild();
+        return command;
+    }
+
+    public static GroupChatCreated personalChatCreatedFrom(CreateGroupChat c) {
+        GroupChatCreated event = GroupChatCreated
+                .newBuilder()
+                .setId(c.getId())
+                .setCreator(c.getCreator())
+                .addAllMember(c.getMemberList())
+                .setName(c.getName())
+                .vBuild();
+        return event;
+    }
+
+    public static Chat chatFrom(CreateGroupChat c) {
+        Chat state = Chat
+                .newBuilder()
+                .setId(c.getId())
+                .setType(GROUP)
+                .addMember(c.getCreator())
+                .addAllMember(c.getMemberList())
+                .setName(c.getName())
+                .vBuild();
+        return state;
+    }
+}
