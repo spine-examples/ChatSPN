dbf8c81cb786307775a3f2ab901ddf5af67ed535
Vladyslav-Kuksiuk <vladyslav.kuksiuk@teamdev.com>
2023-04-10 18:19:40 +0300

    Implement `ChatDeletionProcess`.

---
+++ server/src/main/java/io/spine/examples/chatspn/server/chat/ChatDeletionProcess.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright 2023, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.examples.chatspn.server.chat;
+
+import com.google.common.collect.ImmutableSet;
+import io.spine.client.Filter;
+import io.spine.core.EventContext;
+import io.spine.core.UserId;
+import io.spine.examples.chatspn.ChatDeletionId;
+import io.spine.examples.chatspn.ChatId;
+import io.spine.examples.chatspn.MessageId;
+import io.spine.examples.chatspn.MessageRemovalOperationId;
+import io.spine.examples.chatspn.chat.ChatDeletion;
+import io.spine.examples.chatspn.chat.command.DeleteChat;
+import io.spine.examples.chatspn.chat.command.MarkChatAsDeleted;
+import io.spine.examples.chatspn.chat.event.ChatDeleted;
+import io.spine.examples.chatspn.chat.event.ChatDeletionFailed;
+import io.spine.examples.chatspn.chat.event.ChatMarkedAsDeleted;
+import io.spine.examples.chatspn.chat.rejection.DeletionRejections.ChatCannotBeMarkedAsDeleted;
+import io.spine.examples.chatspn.message.MessageView;
+import io.spine.examples.chatspn.message.command.MarkMessageAsDeleted;
+import io.spine.examples.chatspn.message.event.MessageMarkedAsDeleted;
+import io.spine.examples.chatspn.message.rejection.RemovalRejections.MessageCannotBeMarkedAsDeleted;
+import io.spine.examples.chatspn.server.ProjectionReader;
+import io.spine.server.command.Command;
+import io.spine.server.event.React;
+import io.spine.server.procman.ProcessManager;
+import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
+import static io.spine.client.Filters.eq;
+
+public final class ChatDeletionProcess
+        extends ProcessManager<ChatDeletionId, ChatDeletion, ChatDeletion.Builder> {
+
+    /**
+     * Reads messages per chat.
+     */
+    @MonotonicNonNull
+    private ProjectionReader<MessageId, MessageView> projectionReader;
+
+    @Command
+    MarkChatAsDeleted handle(DeleteChat c) {
+        builder().setId(c.getId());
+        return MarkChatAsDeleted
+                .newBuilder()
+                .setId(chatId(c))
+                .setWhoMarks(c.getWhoDeletes())
+                .vBuild();
+    }
+
+    @Command
+    Iterable<MarkMessageAsDeleted> on(ChatMarkedAsDeleted e, EventContext ctx) {
+        builder().addAllMember(e.getMemberList());
+        Filter byChatId = eq(MessageView.Field.chat(), e.getId());
+        List<MessageView> messages = projectionReader.read(ctx.actorContext(), byChatId);
+        ImmutableSet<MarkMessageAsDeleted> commands =
+                messages.stream()
+                        .map(message -> markMessageAsDeletedCommand(message, e.getWhoDeleted()))
+                        .collect(toImmutableSet());
+        builder().setMessagesToDelete(commands.size());
+        return commands;
+    }
+
+    @React
+    ChatDeletionFailed on(ChatCannotBeMarkedAsDeleted e) {
+        return ChatDeletionFailed
+                .newBuilder()
+                .setId(deletionId(e.getId()))
+                .vBuild();
+    }
+
+    @React
+    Optional<ChatDeleted> on(MessageMarkedAsDeleted e) {
+        return handleMessageDeletion(e.getUser());
+    }
+
+    @React
+    Optional<ChatDeleted> on(MessageCannotBeMarkedAsDeleted e) {
+        return handleMessageDeletion(e.getUser());
+    }
+
+    /**
+     * Archives the process and returns {@link ChatDeleted} event if all message deletions handled.
+     */
+    private Optional<ChatDeleted> handleMessageDeletion(UserId user) {
+        long messages = state().getMessagesToDelete() - 1;
+        builder().setMessagesToDelete(messages);
+        if (messages > 0) {
+            return Optional.empty();
+        }
+        setArchived(true);
+        return Optional.of(chatDeleted(user));
+    }
+
+    private static ChatId chatId(DeleteChat c) {
+        return c.getId()
+                .getId();
+    }
+
+    private static ChatDeletionId deletionId(ChatId id) {
+        return ChatDeletionId
+                .newBuilder()
+                .setId(id)
+                .vBuild();
+    }
+
+    private static MarkMessageAsDeleted
+    markMessageAsDeletedCommand(MessageView message, UserId user) {
+        return MarkMessageAsDeleted
+                .newBuilder()
+                .setId(message.getId())
+                .setChat(message.getChat())
+                .setUser(user)
+                .setProcess(removalOperationId(message.getChat()))
+                .vBuild();
+    }
+
+    private ChatDeleted chatDeleted(UserId user) {
+        return ChatDeleted
+                .newBuilder()
+                .setId(state().getId())
+                .setWhoDeleted(user)
+                .addAllMember(state().getMemberList())
+                .vBuild();
+    }
+
+    private static MessageRemovalOperationId removalOperationId(ChatId id) {
+        return MessageRemovalOperationId
+                .newBuilder()
+                .setChatDeletion(deletionId(id))
+                .vBuild();
+    }
+
+    void inject(ProjectionReader<MessageId, MessageView> reader) {
+        projectionReader = reader;
+    }
+}
---
+++ server/src/main/java/io/spine/examples/chatspn/server/chat/ChatDeletionRepository.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2023, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.examples.chatspn.server.chat;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.errorprone.annotations.OverridingMethodsMustInvokeSuper;
+import io.spine.examples.chatspn.ChatDeletionId;
+import io.spine.examples.chatspn.ChatId;
+import io.spine.examples.chatspn.MessageRemovalOperationId;
+import io.spine.examples.chatspn.chat.ChatDeletion;
+import io.spine.examples.chatspn.chat.event.ChatMarkedAsDeleted;
+import io.spine.examples.chatspn.message.MessageView;
+import io.spine.examples.chatspn.message.event.MessageMarkedAsDeleted;
+import io.spine.examples.chatspn.server.ProjectionReader;
+import io.spine.server.procman.ProcessManagerRepository;
+import io.spine.server.route.EventRouting;
+
+import java.util.Set;
+
+import static io.spine.server.route.EventRoute.withId;
+
+/**
+ * Manages instances of {@link ChatDeletionProcess}.
+ */
+public final class ChatDeletionRepository
+        extends ProcessManagerRepository<ChatDeletionId, ChatDeletionProcess, ChatDeletion> {
+
+    @OverridingMethodsMustInvokeSuper
+    @Override
+    protected void setupEventRouting(EventRouting<ChatDeletionId> routing) {
+        super.setupEventRouting(routing);
+        routing.route(ChatMarkedAsDeleted.class,
+                      (event, context) -> withId(chatDeletion(event.getId())))
+               .route(MessageMarkedAsDeleted.class,
+                      (event, context) -> withChatDeletionId(event.getProcess()));
+    }
+
+    private static Set<ChatDeletionId> withChatDeletionId(MessageRemovalOperationId id) {
+        if (id.hasChatDeletion()) {
+            return ImmutableSet.of(id.getChatDeletion());
+        }
+        return ImmutableSet.of();
+    }
+
+    private static ChatDeletionId chatDeletion(ChatId id) {
+        return ChatDeletionId
+                .newBuilder()
+                .setId(id)
+                .vBuild();
+    }
+
+    @OverridingMethodsMustInvokeSuper
+    @Override
+    protected void configure(ChatDeletionProcess p) {
+        super.configure(p);
+        p.inject(new ProjectionReader<>(context().stand(), MessageView.class));
+    }
+}
