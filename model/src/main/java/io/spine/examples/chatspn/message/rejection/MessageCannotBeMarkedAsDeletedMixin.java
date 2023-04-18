8b7c7255f24fa881c2c0729c3da2dcc16920b3a3
Vladyslav-Kuksiuk <vladyslav.kuksiuk@teamdev.com>
2023-04-18 13:45:33 +0300

    Split `MessageRemovalSignal` into different interfaces.

---
+++ model/src/main/java/io/spine/examples/chatspn/message/rejection/MessageCannotBeMarkedAsDeletedMixin.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2023, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.examples.chatspn.message.rejection;
+
+import com.google.errorprone.annotations.Immutable;
+import io.spine.annotation.GeneratedMixin;
+import io.spine.examples.chatspn.MessageRemovalId;
+import io.spine.examples.chatspn.MessageRemovalOperationId;
+
+/**
+ * Extended interface for {@code MessageCannotBeMarkedAsDeleted} rejection.
+ */
+@Immutable
+@GeneratedMixin
+public interface MessageCannotBeMarkedAsDeletedMixin {
+
+    /**
+     * Returns the ID of the message removal operation.
+     */
+    MessageRemovalOperationId getOperation();
+
+    /**
+     * Returns the ID of the message removal process, or default instance
+     * if message removal operation doesn't have a message removal process.
+     */
+    default MessageRemovalId messageRemoval() {
+        return getOperation().getMessageRemoval();
+    }
+
+    /**
+     * Returns true if a message removal operation has a message removal process.
+     */
+    default boolean hasMessageRemoval() {
+        return getOperation().hasMessageRemoval();
+    }
+}
