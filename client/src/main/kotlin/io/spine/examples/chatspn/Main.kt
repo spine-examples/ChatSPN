1ff6ab6e50c6db2ddf20a3d9bed502dd56c8c225
Vladyslav-Kuksiuk <vladyslav.kuksiuk@teamdev.com>
2023-05-10 13:24:59 +0300

    Change default window state.

--- client/src/main/kotlin/io/spine/examples/chatspn/Main.kt
+++ client/src/main/kotlin/io/spine/examples/chatspn/Main.kt
@@ -24,13 +24,17 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "ChatSPN",
+        state = WindowState(size = DpSize(1000.dp, 600.dp))
     ) {
         App()
     }
