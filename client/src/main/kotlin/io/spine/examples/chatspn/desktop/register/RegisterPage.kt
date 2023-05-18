8ee58bbff45d7d50f04c9b4b94162b9856a67b82
Vladyslav-Kuksiuk <vladyslav.kuksiuk@teamdev.com>
2023-05-18 19:05:42 +0300

    Rewrite `RegistrationPage` using `Form` components.

--- client/src/main/kotlin/io/spine/examples/chatspn/desktop/register/RegisterPage.kt
+++ client/src/main/kotlin/io/spine/examples/chatspn/desktop/register/RegisterPage.kt
@@ -24,42 +24,33 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import io.spine.examples.chatspn.desktop.FormField
+import io.spine.examples.chatspn.desktop.PrimaryButton
+import io.spine.examples.chatspn.desktop.SecondaryButton
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 public fun RegisterPage(
     toLogin: () -> Unit,
     toChats: () -> Unit,
 ) {
-    var email by remember { mutableStateOf("") }
-    var name by remember { mutableStateOf("") }
-    var isEmailError by remember { mutableStateOf(false) }
-    var isNameError by remember { mutableStateOf(false) }
+    val emailState = remember { mutableStateOf("") }
+    val emailErrorState = remember { mutableStateOf(false) }
+    val nameState = remember { mutableStateOf("") }
+    val nameErrorState = remember { mutableStateOf(false) }
 
     Box(
         modifier = Modifier
@@ -70,7 +61,9 @@ public fun RegisterPage(
         Column(
             Modifier
                 .width(500.dp)
-                .padding(10.dp)
+                .padding(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(10.dp)
         ) {
             Text(
                 "Sign Up",
@@ -79,82 +72,16 @@ public fun RegisterPage(
                     .padding(bottom = 20.dp),
                 fontSize = 30.sp,
             )
-            Row {
-                Text(
-                    "Email:",
-                    modifier = Modifier.align(Alignment.CenterVertically).width(50.dp)
-                )
-                TextField(
-                    modifier = Modifier.fillMaxWidth()
-                        .background(MaterialTheme.colorScheme.background)
-                        .padding(start = 10.dp),
-                    value = email,
-                    placeholder = {
-                        Text("john.doe@mail.com")
-                    },
-                    onValueChange = {
-                        email = it
-                        isEmailError = false
-                    },
-                    colors = TextFieldDefaults.textFieldColors(
-                        focusedIndicatorColor = MaterialTheme.colorScheme.primary,
-                        errorIndicatorColor = MaterialTheme.colorScheme.error
-                    ),
-                    isError = isEmailError
-                )
+            FormField("Email:", "john.doe@mail.com", emailState, emailErrorState)
+            FormField("Name:", "John Doe", nameState, nameErrorState)
+            PrimaryButton("Register") {
+                emailErrorState.value = emailState.value.isEmpty()
+                nameErrorState.value = nameState.value.isEmpty()
+                if (!emailErrorState.value && !nameErrorState.value) {
+                    toChats()
+                }
             }
-            Row(Modifier.padding(top = 10.dp)) {
-                Text(
-                    "Name:",
-                    modifier = Modifier.align(Alignment.CenterVertically).width(50.dp)
-                )
-                TextField(
-                    modifier = Modifier.fillMaxWidth()
-                        .background(MaterialTheme.colorScheme.background)
-                        .padding(start = 10.dp),
-                    value = name,
-                    placeholder = {
-                        Text("John Doe")
-                    },
-                    onValueChange = {
-                        name = it
-                        isNameError = false
-                    },
-                    colors = TextFieldDefaults.textFieldColors(
-                        focusedIndicatorColor = MaterialTheme.colorScheme.primary,
-                        errorIndicatorColor = MaterialTheme.colorScheme.error
-                    ),
-                    isError = isNameError
-                )
-            }
-            Button(
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-                    .padding(top = 10.dp),
-                onClick = {
-                    isEmailError = email.isEmpty()
-                    isNameError = name.isEmpty()
-                    if (!isEmailError && !isNameError) {
-                        toChats()
-                    }
-                },
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = MaterialTheme.colorScheme.primary,
-                    contentColor = MaterialTheme.colorScheme.onPrimary
-                )
-            ) {
-                Text("Register")
-            }
-            Button(
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-                    .padding(top = 10.dp),
-                onClick = toLogin,
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color.White,
-                    contentColor = MaterialTheme.colorScheme.primary
-                )
-            ) {
-                Text("Already have an account?")
-            }
+            SecondaryButton("Already have an account?", toLogin)
         }
     }
 }
