ff6c45b750cca9be8a1c63bac8c36080cd64729e
Vladyslav-Kuksiuk <vladyslav.kuksiuk@teamdev.com>
2023-06-16 17:11:38 +0300

    Remove redundant `Font` import.

--- client/src/main/kotlin/io/spine/examples/chatspn/desktop/Theme.kt
+++ client/src/main/kotlin/io/spine/examples/chatspn/desktop/Theme.kt
@@ -33,7 +33,6 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
