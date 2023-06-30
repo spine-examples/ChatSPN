0572a6f3afde1644eee6300829af863c6db3c3c5
Vladyslav-Kuksiuk <vladyslav.kuksiuk@teamdev.com>
2023-06-30 17:38:52 +0300

    Improve the `ChatData` documentation.

--- client/src/main/kotlin/io/spine/examples/chatspn/desktop/navigation/NavigationModel.kt
+++ client/src/main/kotlin/io/spine/examples/chatspn/desktop/navigation/NavigationModel.kt
@@ -207,7 +207,13 @@ public enum class Page {
 }
 
 /**
- * Data for the chat preview.
+ * Data that describes the chat.
+ *
+ * @property id ID of the chat
+ * @property name name of the chat, or name of the chat partner in the personal chat
+ * @property lastMessage last message in the chat
+ * @property members list of the members in the chat
+ * @property type type of the chat
  */
 public data class ChatData(
     val id: ChatId,
